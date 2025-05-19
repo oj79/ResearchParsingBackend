3dc221668a53353d627b0f99431b29e84580e733
owenjiang <cj669@sas.upenn.edu>
2025-05-18 22:22:20 -0400

    Updates

--- parsing/urls.py
+++ parsing/urls.py
@@ -6,5 +6,9 @@ app_name = 'parsing'
 urlpatterns = [
     # Add a URL pattern for your HTML-based parsing view:
     path('parse-references-html/', views.parse_references_html, name='parse_references_html'),
+    path('parse-methods-html/', views.parse_methods_html, name='parse_methods_html'),
     path('upload-form/', views.upload_pdf_form, name='upload_pdf_form'),
+    path('parse-methods-and-tables/', views.parse_methods_and_tables_html, name='parse_methods_and_tables_html'),
+    path('parse-methods-and-tables-summary/', views.parse_methods_and_tables_summarize,
+         name='parse_methods_and_tables_summarize'),
 ]
